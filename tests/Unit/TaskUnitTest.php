c2560dddacd1a3e3e1ae15583ca8829453f36dbb
6210743032 <6210743032@student.tu.ac.th>
2022-04-20 15:01:17 +0700

    taskunittest

---
+++ tests/Unit/TaskUnitTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\Task;
+
+class TaskUnitTest extends TestCase
+{ 
+
+    public function testTaskCreation()
+    {
+
+    }
+
+    //Test Schema that description is correct
+    public function test_schema_description()
+    {
+
+    }
+
+    //Test Schema that User id is correct
+    public function test_schema_userID()
+    {
+
+    }
+    
+}
