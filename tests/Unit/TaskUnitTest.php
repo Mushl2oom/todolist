c5d1ce2ca9d552b0381f3adb9ebd294d5968c4d1
6210743032 <6210743032@student.tu.ac.th>
2022-04-20 15:35:44 +0700

    taskunittest2

--- tests/Unit/TaskUnitTest.php
+++ tests/Unit/TaskUnitTest.php
@@ -8,22 +8,53 @@ use App\Models\Task;
 
 class TaskUnitTest extends TestCase
 { 
+    use RefreshDatabase, Withfaker;
 
-    public function testTaskCreation()
+    public function test_model_user_exists()
     {
+        $user = User::factory()->create();
 
+        $this->assertModelExists($user);
     }
 
-    //Test Schema that description is correct
-    public function test_schema_description()
+    public function test_schema_support_integer()
     {
-
+        $this->assertTrue(true);
     }
 
-    //Test Schema that User id is correct
-    public function test_schema_userID()
+    public function test_schema_detect_sql_injection()
     {
-
+        $this->assertTrue(true);
     }
-    
+
+    public function test_schema_support_length_string()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function test_id_support_integer()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function test_id_detect_null()
+    {
+        $task = new Task([
+            'user_id' => 1,
+            'description' => null,
+        ]);
+        $this->assertEquals(null, $task->description);
+    }
+
+    public function test_route_before_login_exists()
+    {
+        $welcomePage = $this->get('/');
+        $welcomePage->assertStatus(200);
+
+        $loginPage = $this->get('/login');
+        $loginPage->assertStatus(200);
+
+        $registerPage = $this->get('/register');
+        $registerPage->assertStatus(200);
+    }
 }
